237bcb9978a93dedb7f872c34a2d4a42cc1883e2
lmatosevic <lukamatosevic5@gmail.com>
2022-06-07 01:36:23 +0200

    ci: update copy script to create dist if does not exist

--- scripts/copy.js
+++ scripts/copy.js
@@ -1,17 +1,22 @@
 const fs = require('fs');
 
+// Create ./dist directory if it does not exist
+if (!fs.existsSync('./dist')) {
+  fs.mkdirSync('./dist');
+}
+
 // Copy package.json to dist
 let file = fs.readFileSync('package.json').toString();
-fs.writeFileSync('dist/package.json', file);
+fs.writeFileSync('./dist/package.json', file);
 
 // Copy README.md to dist
 file = fs.readFileSync('README.md').toString();
-fs.writeFileSync('dist/README.md', file);
+fs.writeFileSync('./dist/README.md', file);
 
 // Copy LICENSE to dist
 file = fs.readFileSync('LICENSE').toString();
-fs.writeFileSync('dist/LICENSE', file);
+fs.writeFileSync('./dist/LICENSE', file);
 
 // Copy .npmignore to dist
 file = fs.readFileSync('.npmignore').toString();
-fs.writeFileSync('dist/.npmignore', file);
+fs.writeFileSync('./dist/.npmignore', file);
