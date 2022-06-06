f31b31498314900dc5b4062e54cba6bb3ab43039
lmatosevic <lukamatosevic5@gmail.com>
2022-06-07 00:15:25 +0200

    feat: update copy script

--- scripts/copy.js
+++ scripts/copy.js
@@ -1,13 +1,17 @@
 const fs = require('fs');
 
 // Copy package.json to dist
-let resizable = fs.readFileSync('package.json').toString();
-fs.writeFileSync('dist/package.json', resizable);
+let file = fs.readFileSync('package.json').toString();
+fs.writeFileSync('dist/package.json', file);
 
 // Copy README.md to dist
-resizable = fs.readFileSync('README.md').toString();
-fs.writeFileSync('dist/README.md', resizable);
+file = fs.readFileSync('README.md').toString();
+fs.writeFileSync('dist/README.md', file);
 
 // Copy LICENSE to dist
-resizable = fs.readFileSync('LICENSE').toString();
-fs.writeFileSync('dist/LICENSE', resizable);
+file = fs.readFileSync('LICENSE').toString();
+fs.writeFileSync('dist/LICENSE', file);
+
+// Copy .npmignore to dist
+file = fs.readFileSync('.npmignore').toString();
+fs.writeFileSync('dist/.npmignore', file);
