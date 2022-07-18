9c905074a352599aceecc71b8a46142c009f364a
lmatosevic <lukamatosevic5@gmail.com>
2022-07-19 01:31:01 +0200

    fix: roles decorator now works together with authorized decorator

--- lib/auth/decorators/roles.decorator.ts
+++ lib/auth/decorators/roles.decorator.ts
@@ -1,6 +1,9 @@
-import { SetMetadata } from '@nestjs/common';
 import { Role } from '../../enums';
 
 export const ROLES_KEY = 'roles';
 
-export const Roles = (...roles: Role[]) => SetMetadata(ROLES_KEY, roles);
+export const Roles = (...roles: Role[]) => {
+  return (target: Object, _: string) => {
+    Reflect.defineMetadata(ROLES_KEY, roles, target);
+  }
+}
