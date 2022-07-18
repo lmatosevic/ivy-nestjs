400eeba06bfebee24002e0286dcf8780ad9aace1
lmatosevic <lukamatosevic5@gmail.com>
2022-07-19 01:35:54 +0200

    chore: update sample projects worker endpoint

--- sample/typeorm-app/src/modules/worker/worker.controller.ts
+++ sample/typeorm-app/src/modules/worker/worker.controller.ts
@@ -1,13 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { InjectQueue } from '@nestjs/bull';
 import { Queue } from 'bull';
-import { StatusResponse } from 'ivy-nestjs';
+import { Authorized, Role, Roles, StatusResponse } from 'ivy-nestjs';
 import { WorkerData } from './worker.job';
 
 @Controller('worker')
 export class WorkerController {
   constructor(@InjectQueue('worker') private workerQueue: Queue<WorkerData>) {}
 
+  @Authorized()
+  @Roles(Role.Admin)
   @Get('start')
   async start(): Promise<StatusResponse> {
     await this.workerQueue.add({ startedAt: new Date() });
