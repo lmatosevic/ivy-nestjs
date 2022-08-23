24b48a367813c50d19914239da85bbfcb0d22e27
lmatosevic <lukamatosevic5@gmail.com>
2022-08-23 09:15:51 +0200

    refactor: revert protected service to private access

--- lib/resource/helpers/resource-controller.helper.ts
+++ lib/resource/helpers/resource-controller.helper.ts
@@ -227,7 +227,7 @@ export function ResourceController<T extends Type<unknown>, C extends Type<unkno
   @ApiTags(pluralName)
   @Resource(config)
   abstract class ResourceController {
-    protected readonly protectedService: ResourceService<T>;
+    private readonly protectedService: ResourceService<T>;
 
     protected constructor(
       protected service: ResourceService<T>,
